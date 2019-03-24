20b1edf8d21cc7053d2b1ce178375ea4480ded2c
WarsFeng <huangrongfeng1999@gmail.com>
2019-03-24 23:45:16 +0800

    refactor(api, page): Page list update
    add site and pageAlias conditional matching

--- src/module/cms/api/cms.js
+++ src/module/cms/api/cms.js
@@ -1,8 +1,15 @@
-import http from "../../../base/api/public"
+import http from "../../../base/api/public";
+import querystring from "querystring";
 
 let sysConfig = require('@/../config/sysConfig');
 let apiUrl = sysConfig.cmsApiUrlPre;
 
 export const page_list = (page, size, params) => {
-  return http.requestQuickGet(apiUrl+"/cms/page/list/" + page + "/" + size);
+  let query = querystring.stringify(params);
+  return http.requestQuickGet(apiUrl + "/cms/page/list/" + page + "/" + size + "?" + query);
+};
+
+export const site_sublist = (siteName) => {
+  let query = querystring.stringify({siteName});
+  return http.requestQuickGet(apiUrl + "/cms/site/sublist?" + query);
 };
--- src/module/cms/page/page_list.vue
+++ src/module/cms/page/page_list.vue
@@ -1,10 +1,32 @@
 <template>
   <div>
-    <el-button
-      @click="query"
-      type="primary" icon="el-icon-search" size="small"
-      :loading=loading>搜索
-    </el-button>
+    <!-- Header form -->
+    <el-form :inline="true" :model="params" class="demo-form-inline">
+      <el-form-item label="站点列表:">
+        <el-select
+          v-model="params.siteList"
+          filterable remote clearable
+          placeholder="请选择站点"
+          :remote-method="siteRemoteMethod"
+          :loading="loading.site">
+          <el-option
+            v-for="item in siteList"
+            :key="item.siteId"
+            :label="item.siteName"
+            :value="item.siteId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="页面名称:">
+        <el-input placeholder="页面名称" v-model="params.pageAlias"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="small"
+                   :loading="loading.main" @click="query()">查询
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <!-- Content table -->
     <el-table
       :data="list"
       stripe
@@ -41,12 +63,13 @@
         width="280">
       </el-table-column>
     </el-table>
+    <!-- Footer pagination -->
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page=params.page
-      :page-sizes="[params.size, params.size*2, params.size*3, params.size*4]"
-      :page-size=params.size
+      :current-page=page
+      :page-sizes="[size, size*2, size*3, size*4]"
+      :page-size=size
       layout="total, sizes, prev, pager, next, jumper"
       :total=total
       style="text-align: center"
@@ -63,35 +86,53 @@
     data() {
       return {
         list: [],
-        total: 550,
-        loading: false,
+        total: 0,
+        page: 1, // 页码
+        size: 20,// 每页数量
+        // Query by other param
         params: {
-          page: 1, // 页码
-          size: 20 // 每页数量
+          siteId: '',
+          pageAlias: '',
+        },
+        // Site list
+        siteList: [],
+        // Loading
+        loading: {
+          main: false,
+          site: false
         }
       }
     },
     methods: {
-      handleSizeChange(val) {
-        this.params.size = val;
+      handleSizeChange(size) {
+        this.size = size;
         this.query();
       },
-      handleCurrentChange(val) {
-        this.params.page = val;
+      handleCurrentChange(page) {
+        this.page = page;
         this.query();
+      },
+      siteRemoteMethod(siteName) {
+        this.loading.site = true;
+        setTimeout(() => {
+          this.siteList = cmsApi.site_sublist(siteName).then(res => {
+            this.siteList = res.queryResult.list;
+            this.loading.site = false;
+          });
+        }, 200);
       },
       query() {
-        this.loading = true;
-        cmsApi.page_list(this.params.page, this.params.size, this.params).then(res => {
-          console.log(res);
+        this.loading.main = true;
+        cmsApi.page_list(this.page, this.size, this.params).then(res => {
           this.list = res.queryResult.list;
           this.total = res.queryResult.total;
-          this.loading = false;
+          this.loading.main = false;
         });
       }
     },
     created() {
       this.query();
+      this.siteRemoteMethod();
     }
   }
 </script>
